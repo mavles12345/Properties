1377b3c23cfab253fb59887bcd65959eb0df66d2
mavles12345 <76029496+mavles12345@users.noreply.github.com>
2021-05-07 10:56:59 +0530

    Properties

---
+++ src/test/java/com/testcase/PropertiesRead.java
@@ -0,0 +1,34 @@
+package com.testcase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PropertiesRead {
+
+	public static void main(String[] args) throws IOException {
+
+		Properties p = new Properties();
+
+		FileInputStream file = new FileInputStream("Configurarion.properties");
+
+		p.load(file);
+
+		System.setProperty("webdriver.chrome.driver", p.getProperty("driverlocation"));
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys(p.getProperty("username"));
+		driver.findElement(By.id("pass")).sendKeys(p.getProperty("password"));
+		driver.findElement(By.name("login")).click();
+
+	}
+
+}
